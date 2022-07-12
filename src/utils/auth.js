7988aef4f79f1f825506360bc84920b08319bddc
LuoMiao <1084459061@qq.com>
2022-07-12 10:30:26 +0800

    feat: login完成

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,8 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
+const TokenKey = 'hrsaas-ihrm-token' // 设定一个独一无二的key
+
+const timeKey = 'hr-sass-time-key'
 
 export function getToken() {
   return Cookies.get(TokenKey)
@@ -12,4 +14,13 @@ export function setToken(token) {
 
 export function removeToken() {
   return Cookies.remove(TokenKey)
+}
+
+export function setTimeStamp() {
+  // 设置当前时间戳
+  Cookies.set(timeKey, Date.now())
+}
+
+export function getTimeStamp() {
+  return Cookies.get(timeKey)
 }
